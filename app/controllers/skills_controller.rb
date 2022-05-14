71916e28a4e74ff0c5a35db1882502d1334c5d84
Erick Escobar <90279262+eescobarvazquez@users.noreply.github.com>
2022-05-13 19:13:26 -0500

    Week 1 Assignment

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,9 +1,12 @@
 class SkillsController < ApplicationController
 
   def check
+    #Instance variables that can be passed towards the view. Note the @ symbol in the name
+    @guest = 'Cody'
   end
 
   def skills
+    @ur_skills = params[:skills].split(',')
   end
 
 end
